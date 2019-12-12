ec4905f82d89f3db2633a32f762832cb696049cf
Jarosław Zabuski <45364202+berochel@users.noreply.github.com>
2019-12-12 18:12:18 +0100

    Usuwanie wezla z drzewa BST

---
+++ prog.cpp
@@ -0,0 +1,201 @@
+#include <iostream>
+#include "test.h"
+struct nod
+{
+    int liczba;
+    nod *lewy, *prawy;
+    nod (int n) : liczba(n), lewy(nullptr), prawy(nullptr) {}
+    ~nod () = default;
+};
+class BST
+{
+    public:
+    nod *korzen;
+    void insert (int n)
+    {
+        if (!korzen)
+        {
+            nod *temp = new nod(n);
+            korzen = temp;
+        }
+        else
+        {
+            nod *temp = korzen;
+            nod *temp2 = nullptr;
+            bool side = false;
+            while (temp)
+            {
+                temp2 = temp;
+                if (temp->liczba < n)
+                {
+                    temp = temp->prawy;
+                    side = true;
+                }
+                else
+                {
+                    temp = temp->lewy;
+                    side = false;
+                }
+            }
+            temp = new nod(n);
+            if (side)
+            {
+                temp2->prawy = temp;
+            } else
+            {
+                temp2->lewy = temp;
+            }
+        }
+    }
+    void deletenod (int n)
+    {
+        if (!korzen)
+        {
+            std::cout << "Tree is empty" << std::endl;
+        }
+        else
+        {
+            nod *temp = korzen;
+            nod *temp2 = nullptr;
+            bool side = false;
+            while (temp)
+            {
+                if (temp->liczba != n)
+                {
+                    temp2 = temp;
+                    if (temp->liczba > n)
+                    {
+                        temp = temp->lewy;
+                        side = true;
+                    }
+                    else
+                    {
+                        temp = temp->prawy;
+                        side = false;
+                    }
+                }
+                else
+                {
+                    if (!temp->lewy && !temp->prawy)
+                    {
+                        if (!side)
+                        {
+                            temp2->lewy = NULL;
+                        } else
+                        {
+                            temp2->prawy = NULL;
+                        }
+                        break;
+                    }
+                    else if (!temp->prawy && temp->lewy)
+                    {
+                        if (!side)
+                        {
+                            temp2->lewy = temp->lewy;
+                        }
+                        else
+                        {
+                            temp2->prawy = temp->lewy;
+                        }
+                        break;
+                    }
+                    else if (!temp->lewy && temp->prawy)
+                    {
+                        if (!side)
+                        {
+                            temp2->lewy = temp->prawy;
+                        }
+                        else
+                        {
+                            temp2->prawy = temp->prawy;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        int key = findSmallestnod(temp);
+                        deletenod(key);
+                        temp->liczba = key;
+                        break;
+                    }
+                }
+            }
+            if (!temp)
+            {
+                std::cout << "Key -> " << n << " not found" << std::endl;
+            }
+            else
+            {
+                temp = nullptr;
+            }
+        }
+    }
+    bool contains(int n)
+    {
+        nod *temp = korzen;
+        while (temp->liczba != n)
+        {
+            if (temp->liczba > n)
+            {
+                temp = temp->lewy;
+            }
+            else
+            {
+                temp = temp->prawy;
+            }
+            if (temp == NULL)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    void sizeCount(nod* korzen, int &sum) const
+    {
+        if (korzen!=NULL)
+        {
+              ++sum;
+              sizeCount(korzen->lewy, sum);
+              sizeCount(korzen->prawy, sum);
+        }
+    }
+    int size() const
+    {
+        int sum=0;
+        if (this->korzen!=NULL)
+        {
+              ++sum;
+              sizeCount(korzen->lewy, sum);
+              sizeCount(korzen->prawy, sum);
+        }
+        return sum;
+    }
+    int findSmallestnod(nod *korzen)
+    {
+        nod *temp = korzen->prawy;
+
+        while (temp && temp->lewy != NULL)
+        {
+            temp= temp->lewy;
+        }
+        return temp->liczba;
+    }
+    void display(nod* ptr, int poziom)
+    {
+        if (ptr)
+        {
+            display(ptr->prawy, poziom + 1);
+            printf("\n");
+            if (ptr == korzen)
+            std::cout << "Korzen drzewa BST -> ";
+            for (int i = 0; i < poziom && ptr != korzen; i++)
+                std::cout << "      ";
+            std::cout << ptr->liczba << std::endl;
+            display(ptr->lewy, poziom + 1);
+        }
+    }
+};
+int main()
+{
+    unit_test();
+}
---
+++ test.h
@@ -0,0 +1,43 @@
+#include <cassert>
+void unit_test() {
+
+    BST tree;
+
+    tree.insert(4);
+    tree.insert(43);
+    tree.insert(18);
+    tree.insert(5);
+    tree.insert(8);
+    tree.insert(56);
+    tree.insert(10);
+    tree.insert(14);
+    tree.insert(56);
+    tree.insert(9);
+    assert(tree.size() == 10);
+    tree.insert(42);
+    assert(tree.contains(3) == false);
+    assert(tree.contains(56) == true);
+    assert(tree.size() == 11);
+    tree.insert(34);
+    tree.insert(72);
+    tree.insert(26);
+
+    tree.deleteNode(8);
+    assert(tree.root->right->data == 9);
+    tree.deleteNode(10);
+    assert(tree.root->right->right->data == 14);
+
+    //usuwany roota
+    tree.deleteNode(4);
+    assert(tree.root->data == 5);
+
+    //usuwamy element ktorego nie ma w drzewie
+    assert(tree.size() == 11);
+    tree.deleteNode(88);
+    assert(tree.size() == 11);
+
+    //kontrolne reczne rysowanie drzewa
+    tree.display(tree.korzen, 1);
+
+    std::cout << "testy zdane!";
+}
